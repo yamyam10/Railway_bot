89c3eacd0a2e58a07ae129dbdb2e51e6b1686e69
Niiii <tapioka2real@gmail.com>
2024-06-07 15:28:26 +0900

    Remove .env file from repository and add to .gitignore

---
+++ main.py
@@ -0,0 +1,26 @@
+import discord
+import os
+from dotenv import load_dotenv
+from discord.ext import commands
+
+load_dotenv()
+TOKEN = os.getenv('kani_TOKEN')  # 🦀bot
+# TOKEN = os.getenv('cps_TOKEN')  # カスタム大会bot
+
+intents = discord.Intents.default()
+intents.message_content = True  # メッセージコンテンツの意図を有効にする
+
+bot = commands.Bot(command_prefix='!', intents=intents)
+
+@bot.event
+async def on_ready():
+    print(f'We have logged in as {bot.user}')
+
+@bot.event
+async def on_message(message):
+    if message.author == bot.user:
+        return
+
+    await message.channel.send(message.content)
+
+bot.run(TOKEN)
